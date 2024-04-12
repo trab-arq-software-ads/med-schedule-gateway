ad93f97518010129292429c7894981f39830aa09
DamiaoFilho <damiaofilho0123@gmail.com>
2024-04-12 19:18:29 -0300

    config port

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.use(appointmentsRouter);
 app.use(doctorsRouter);
 app.use(patientsRouter);
 
-app.listen(3000, function () {
-  console.log("Controller Service running on port 3000");
+app.listen(3001, function () {
+  console.log("Controller Service running on port 3001");
 });
